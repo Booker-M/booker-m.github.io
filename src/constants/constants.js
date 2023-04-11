a5c100e94ee2ada8254f4a6d8e77ed2639179e5c
Booker-M <bookerjmartin@gmail.com>
2023-04-11 13:46:21 -0700

    Updated to use GitHub pages

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,119 +1,147 @@
 export const games = [
   {
-    title: 'Fractured Sky',
-    description: "Fractured Sky is an innovative, upcoming tactical role-playing game in which player choice impacts character relationships and narrative outcomes. The game makes use of procedural-generation to create levels that are entirely unique every playthrough and fully destructible.",
-    image: '/images/FracturedSky.png',
-    tags: ['Unity', 'C#'],
-    source: 'https://fracturedsky.ml',
-    visit: 'https://fracturedsky.ml',
+    title: "Fractured Sky",
+    description:
+      "Fractured Sky is an innovative, upcoming tactical role-playing game in which player choice impacts character relationships and narrative outcomes. The game makes use of procedural-generation to create levels that are entirely unique every playthrough and fully destructible.",
+    image: "/images/FracturedSky.png",
+    tags: ["Unity", "C#"],
+    source: "https://fracturedsky.github.io/",
+    visit: "https://fracturedsky.github.io/",
     id: 0,
   },
   {
-    title: 'Star Fleet',
-    description: "Star Fleet is an intense space dog-fighting game. Lead your fleet of ships to victory by defeating all enemy ships. Each wave brings a new challenge as the number of enemy ships and teams increases. Do you have what it takes to be the best pilot in the galaxy?",
-    image: '/images/StarFleet.png',
-    tags: ['Unreal Engine', 'C++', 'Blueprints'],
-    source: 'https://drive.google.com/drive/folders/1CbBlW_GeLaD_uDI5eo1mE1PYoi6E3DHc?usp=sharing',
-    visit: 'https://drive.google.com/drive/folders/1CbBlW_GeLaD_uDI5eo1mE1PYoi6E3DHc?usp=sharing',
+    title: "Star Fleet",
+    description:
+      "Star Fleet is an intense space dog-fighting game. Lead your fleet of ships to victory by defeating all enemy ships. Each wave brings a new challenge as the number of enemy ships and teams increases. Do you have what it takes to be the best pilot in the galaxy?",
+    image: "/images/StarFleet.png",
+    tags: ["Unreal Engine", "C++", "Blueprints"],
+    source:
+      "https://drive.google.com/drive/folders/1CbBlW_GeLaD_uDI5eo1mE1PYoi6E3DHc?usp=sharing",
+    visit:
+      "https://drive.google.com/drive/folders/1CbBlW_GeLaD_uDI5eo1mE1PYoi6E3DHc?usp=sharing",
     id: 0,
   },
-];
+]
 
 export const webGames = [
   {
-    title: 'Space Cadet',
-    description: "An arcade-style space game with a modern twist: planets exert gravitional pull on their surroundings. Explore the final frontier!",
-    image: '/images/SpaceCadet.png',
-    tags: ['JavaScript', 'p5.js'],
-    source: 'https://github.com/Booker-M/Space-Cadet',
-    visit: 'https://booker-m.github.io/Space-Cadet/',
+    title: "Space Cadet",
+    description:
+      "An arcade-style space game with a modern twist: planets exert gravitional pull on their surroundings. Explore the final frontier!",
+    image: "/images/SpaceCadet.png",
+    tags: ["JavaScript", "p5.js"],
+    source: "https://github.com/Booker-M/Space-Cadet",
+    visit: "https://booker-m.github.io/Space-Cadet/",
     id: 0,
   },
   {
-    title: 'Coral Planet',
-    description: "Step into the center of the world where a variety of sea creatures roam around the planet’s core. Your job: pick up the pollution.",
-    image: '/images/CoralPlanet.png',
-    tags: ['React.js', 'Three.js'],
-    source: 'https://github.com/Booker-M/Coral-Planet',
-    visit: 'https://booker-m.github.io/Coral-Planet/',
+    title: "Coral Planet",
+    description:
+      "Step into the center of the world where a variety of sea creatures roam around the planet’s core. Your job: pick up the pollution.",
+    image: "/images/CoralPlanet.png",
+    tags: ["React.js", "Three.js"],
+    source: "https://github.com/Booker-M/Coral-Planet",
+    visit: "https://booker-m.github.io/Coral-Planet/",
     id: 1,
   },
   {
-    title: 'Deep Space',
-    description: "A deep-space adventurer is in uncharted territory searching for crystals when they are ambushed by an alien fleet.",
-    image: '/images/DeepSpace.png',
-    tags: ['React.js', 'Three.js'],
-    source: 'https://github.com/Booker-M/Deep-Space',
-    visit: 'https://booker-m.github.io/Deep-Space/',
+    title: "Deep Space",
+    description:
+      "A deep-space adventurer is in uncharted territory searching for crystals when they are ambushed by an alien fleet.",
+    image: "/images/DeepSpace.png",
+    tags: ["React.js", "Three.js"],
+    source: "https://github.com/Booker-M/Deep-Space",
+    visit: "https://booker-m.github.io/Deep-Space/",
     id: 2,
   },
-];
+]
 
 export const webApps = [
   {
-    title: 'Parley - Language Translation',
-    description: "Parley is a messaging app that aims to foster a global community of users able to click on messages to translate them to their target language.",
-    image: '/images/Parley.png',
-    tags: ['React.js', 'Firebase', 'Google Translate API'],
-    source: 'https://github.com/Booker-M/parley',
-    visit: 'https://booker-m.github.io/parley',
+    title: "Parley - Language Translation",
+    description:
+      "Parley is a messaging app that aims to foster a global community of users able to click on messages to translate them to their target language.",
+    image: "/images/Parley.png",
+    tags: ["React.js", "Firebase", "Google Translate API"],
+    source: "https://github.com/Booker-M/parley",
+    visit: "https://booker-m.github.io/parley",
     id: 0,
   },
   {
-    title: 'Custom - Programming Language',
-    description: "Custom is a programming language that allows users to define their unique version of the language in a customizable JSON. Each person’s version of Custom is truly their own.",
-    image: '/images/Custom.png',
-    tags: ['JavaScript', 'Ohm'],
-    source: 'https://github.com/Booker-M/Custom',
-    visit: 'https://booker-m.github.io/Custom/',
+    title: "Custom - Programming Language",
+    description:
+      "Custom is a programming language that allows users to define their unique version of the language in a customizable JSON. Each person’s version of Custom is truly their own.",
+    image: "/images/Custom.png",
+    tags: ["JavaScript", "Ohm"],
+    source: "https://github.com/Booker-M/Custom",
+    visit: "https://booker-m.github.io/Custom/",
     id: 0,
   },
-];
+]
 
 export const filters = [
   {
-    title: 'Cyberpunk Showdown',
-    video: 'https://community-lens.storage.googleapis.com/preview-media/final/6c89b3e1-5f9f-4bdf-93c6-2266a0c0c9a1.mp4',
-    visit: 'https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=89805bd71f2f4300bae4bf9d9102d83a&metadata=01',
+    title: "Cyberpunk Showdown",
+    video:
+      "https://community-lens.storage.googleapis.com/preview-media/final/6c89b3e1-5f9f-4bdf-93c6-2266a0c0c9a1.mp4",
+    visit:
+      "https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=89805bd71f2f4300bae4bf9d9102d83a&metadata=01",
     id: 0,
   },
   {
-    title: 'Post-Apocalyptic',
-    video: 'https://community-lens.storage.googleapis.com/preview-media/final/382ecb6f-c474-40e5-b153-163e09f8b3b2.mp4',
-    visit: 'https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=b079483d8b274676a6010111797b6507&metadata=01',
+    title: "Post-Apocalyptic",
+    video:
+      "https://community-lens.storage.googleapis.com/preview-media/final/382ecb6f-c474-40e5-b153-163e09f8b3b2.mp4",
+    visit:
+      "https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=b079483d8b274676a6010111797b6507&metadata=01",
     id: 1,
   },
   {
-    title: 'Lucid Geode World',
-    video: 'https://community-lens.storage.googleapis.com/preview-media/final/4a2ae1da-6288-4211-b9c2-ebb766a88e4b.mp4',
-    visit: 'https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=bafbe86c988e466a985ed3761bf412bb&metadata=01',
+    title: "Lucid Geode World",
+    video:
+      "https://community-lens.storage.googleapis.com/preview-media/final/4a2ae1da-6288-4211-b9c2-ebb766a88e4b.mp4",
+    visit:
+      "https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=bafbe86c988e466a985ed3761bf412bb&metadata=01",
     id: 2,
   },
   {
-    title: 'Black Knight Fight',
-    video: 'https://community-lens.storage.googleapis.com/preview-media/final/d2b44aea-2f7c-4f12-86a3-5dbedca3711f.mp4',
-    visit: 'https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=e7dc6f612b494944a7218496dcafdcb7&metadata=01',
+    title: "Black Knight Fight",
+    video:
+      "https://community-lens.storage.googleapis.com/preview-media/final/d2b44aea-2f7c-4f12-86a3-5dbedca3711f.mp4",
+    visit:
+      "https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=e7dc6f612b494944a7218496dcafdcb7&metadata=01",
     id: 3,
   },
   {
-    title: 'Darth Vader - Lava',
-    video: 'https://community-lens.storage.googleapis.com/preview-media/final/1a85b3bd-2dcd-4ba1-b5d2-3dedff27e862.mp4',
-    visit: 'https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=eb58566790ad46c398ee2969eeb395b0&metadata=01',
+    title: "Darth Vader - Lava",
+    video:
+      "https://community-lens.storage.googleapis.com/preview-media/final/1a85b3bd-2dcd-4ba1-b5d2-3dedff27e862.mp4",
+    visit:
+      "https://www.snapchat.com/unlock/?type=SNAPCODE&uuid=eb58566790ad46c398ee2969eeb395b0&metadata=01",
     id: 4,
   },
-];
+]
 
 export const TimeLineData = [
-  { year: 2018, text: 'Began undergrad at Loyola Marymount University', },
-  { year: 2019, text: 'Taught grades 5-8 as a Coding Instructor', },
-  { year: 2020, text: 'Received the Honors Fellowship Research Grant for two projects', },
-  { year: 2021, text: 'Joined Dolby Labs as an Enterprise Application Integrations intern', },
-  { year: 2022, text: 'Started development on solo indie game "Fractured Sky"', },
-];
+  { year: 2018, text: "Began undergrad at Loyola Marymount University" },
+  { year: 2019, text: "Taught grades 5-8 as a Coding Instructor" },
+  {
+    year: 2020,
+    text: "Received the Honors Fellowship Research Grant for two projects",
+  },
+  {
+    year: 2021,
+    text: "Joined Dolby Labs as an Enterprise Application Integrations intern",
+  },
+  {
+    year: 2022,
+    text: 'Started development on solo indie game "Fractured Sky"',
+  },
+]
 
 export const AcomplishmentsData = [
-  { number: "3.98 GPA", text: 'with Honors'},
-  { number: "Dean's List", text: 'Fall 2018 - Fall 2021', },
-  { number: "Research Fellowship Grant", text: '', },
-  { number: "Presidential Scholarship", text: '', }
-];
+  { number: "3.98 GPA", text: "with Honors" },
+  { number: "Dean's List", text: "Fall 2018 - Fall 2021" },
+  { number: "Research Fellowship Grant", text: "" },
+  { number: "Presidential Scholarship", text: "" },
+]
